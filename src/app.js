239cc16d90331fd45f8ef22cf45e3a94155404b9
Unknown <musashi.miyamoto81@gmail.com>
2018-03-04 14:27:27 +0100

    [paper] import paper-input (converted with modulizer)

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
 // Element is the same as Polymer.Element in 2.x
 // Modules give you the freedom to rename the members that you import
 import {Element as PolymerElement} from '../node_modules/@polymer/polymer/polymer-element.js';
+import { PaperInput } from '../node_modules/@polymer/paper-input/paper-input.js';
 
 // Added "export" to export the MyApp symbol from the module
 export class App extends PolymerElement {
 
   // Define a string template instead of a `<template>` element.
   static get template() {
-    return `<div>This is my [[name]] app.</div>`
+    return `<div>This is my [[name]] app.</div><paper-input always-float-label label="Floating label"></paper-input>`
   }
 
   constructor() {
